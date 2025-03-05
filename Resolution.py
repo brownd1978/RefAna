169245e4cec744d1da314e162b4a2d670099b40c
David Nathan Brown <dave_brown@lbl.gov>
2025-03-04 22:43:34 -0800

    Add track quality cuts; prep for batch multi-file mode

--- Resolution.py
+++ Resolution.py
@@ -9,24 +9,43 @@ import uproot
 #file = '/home/online1/ejc/public/brownd/dts.mu2e.CeEndpoint.MDC2020r.001210_00000000.art.digi.art.ntuple.root'
 #file = '/data/HD5/users/brownd/ntp.brownd.Reflections.v4.root'
 file = "/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00000000.root"
+files = [
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00000000.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00000010.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00000024.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00000053.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00000085.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00004963.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00005432.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00010057.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00010872.root:TAReM/ntuple",
+"/data/HD5/users/brownd/71077187/nts.brownd.TAReflect.TARef.001202_00015026.root:TAReM/ntuple" ]
+
+
+
 with uproot.open(file) as f:
+#for batch,rep in uproot.iterate(files,filter_name=\"/trk|trksegs|trkmcsim/i\",report=True):\n",
     tree = f['TAReM']['ntuple']
     segs = tree['trksegs'].array(library='ak') # track fit samples
-    trks = tree['trk'].array(library='ak') # track fit
+    nhits = tree['trk.nactive'].array(library='ak') # track
+    fitcon = tree['trk.fitcon'].array(library='ak') # track fit
     trkMC = tree['trkmcsim'].array(library='ak') # MC genealogy of particles
     trkSegMC = tree['trksegsmc'].array(library='ak') # SurfaceStep infor for true primary particle
-#   # sanity check that branches are aligned
-    assert(len(segs) == len(trks))
 #    ak.type(segs).show()
 #    print("segs axis 0: ",ak.num(segs,axis=0))
 #    print("segs axis 1: ",ak.num(segs,axis=1))
 #    print("segs axis 2: ",ak.num(segs,axis=2))
     upSegs = segs[:,0] # upstream track fits
     dnSegs = segs[:,1] # downstream track fits
-    upTrks = trks[:,0]
-    dnTrks = trks[:,1]
-#    ak.type(upTrks).show()
-#    print(upTrks)
+#    print(len(fitcon), fitcon)
+    upFitCon = fitcon[:,0]
+    dnFitCon = fitcon[:,1]
+    upNhits = nhits[:,0]
+    dnNhits = nhits[:,1]
+    print(len(upFitCon),upFitCon)
+    print(len(dnFitCon),dnFitCon)
+    print(len(upNhits),upNhits)
+    print(len(dnNhits),dnNhits)
 
 
 # select based on time difference at tracker entrance
@@ -40,6 +59,8 @@ with uproot.open(file) as f:
     dnTrkMC = dnTrkMC[dnTrkMC.trkrel._rel == 0]
     upTrkMC = ak.flatten(upTrkMC,axis=1) # project out the struct
     dnTrkMC = ak.flatten(dnTrkMC,axis=1)
+
+#    print( len(upTrkMC))
 
     upElMC = upTrkMC.pdg == 11
     dnElMC = dnTrkMC.pdg == 11
@@ -59,10 +80,12 @@ with uproot.open(file) as f:
     goodele = ak.flatten(goodele)
 #    print(goodele,len(goodele))
 # select based on fit quality
-#    upGoodFit = (upTrks."trk.nactive" > 19) & (upTrks."trk.fitcon" > 1.0e-3)
-#    dnGoodFit = (dnTrks."trk.nactive" > 19) & (dnTrks."trk.fitcon" > 1.0e-3)
-#    goodfit = upGoodFit & dnGoodFit
-#    print(goodfit,len(goodfit))
+    minNHits = 20
+    minFitCon = 1.0e-5
+    upGoodFit = (upNhits >= minNHits) & (upFitCon > minFitCon)
+    dnGoodFit = (dnNhits >= minNHits) & (dnFitCon > minFitCon)
+    goodfit = upGoodFit & dnGoodFit
+    print(goodfit,len(goodfit))
 
     # sample the fits at middle of traacker
     upMidSegs = upSegs[upSegs.sid==1]
@@ -75,14 +98,20 @@ with uproot.open(file) as f:
     upMidMom = ak.ravel(upMidMom)
     dnMidMom = ak.ravel(dnMidMom)
 
-
-    # select
-    upMidMom = upMidMom[goodele]
-    dnMidMom = dnMidMom[goodele]
+    # select: first PID and fit quality
+    upMidMom = upMidMom[goodele & goodfit]
+    dnMidMom = dnMidMom[goodele & goodfit]
 
     # momentum range around a conversion electron
-    signalLike = (dnMidMom > 90) & (dnMidMom < 120)
+    cemom = 104
+    dmom = 20
+    minmom = cemom - dmom
+    maxmom = cemom + dmom
+    signalLike = (dnMidMom > minmom) & (dnMidMom < maxmom)
     # good quality tracks
+
+#    upGoodFit = (nhits[0] > 19) & (fitcon[0] > 1e-4)
+#    print(upGoodFit)
 
     deltaMidMom = upMidMom - dnMidMom
     deltaMidMom = deltaMidMom[signalLike]
